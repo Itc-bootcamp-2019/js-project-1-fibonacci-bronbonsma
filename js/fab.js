f532807b97efd565b6116a863d8d60393f308880
Bronwyn Bonsma <babonsma@gmail.com>
2020-03-01 20:53:31 +0200

    Milestone 3

--- js/fab.js
+++ js/fab.js
@@ -1,5 +1,15 @@
-
-document.getElementById('x').innerText = "4";
-document.getElementById('numberlist').innerText = "3";
-
-
+function fib(x) {
+  let prev1 = 1;
+  let prev2 = 0;
+  for (let i = 1; i < x + 1; i++) {
+    y = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = y;
+  }
+  return y;
+}
+x = 5;
+console.log(fib(x));
+y = fib(x);
+document.getElementById("x").innerText = [x];
+document.getElementById("y").innerText = [y];
