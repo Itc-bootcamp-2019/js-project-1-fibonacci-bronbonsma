288bb9c4ab6f3da78b3f8f0442e4fe4bea2ecb1d
Bronwyn Bonsma <babonsma@gmail.com>
2020-03-01 17:35:21 +0200

    milestone 1 complete

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,4 @@
+function math(myAge, yourAge, hisAge) {
+  return yourAge * hisAge + myAge;
+}
+console.log(math(53, 61, 67));
---
+++ javascripting/if-statement.js
@@ -0,0 +1,8 @@
+let fruit = "orange";
+let orange = 6;
+
+if (orange >= 5) {
+  console.log("The fruit name has more than five characters.");
+} else {
+  console.log("The fruit name has five characters or less.");
+}
