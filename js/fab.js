b89c004ff2f9e1c56b64a618008001dd3b08fd8a
Bronwyn Bonsma <babonsma@gmail.com>
2020-03-03 15:53:32 +0200

    Milestone 4.1

--- js/fab.js
+++ js/fab.js
@@ -1,13 +1,20 @@
-function fib(x) {
-  let a = 0;
-  let b = 1;
-  let result = 0;
-  for (let i = 2; i <= x; i++) {
-    result = a + b;
-    a = b;
-    b = result;
-  }
-  return result;
+// function fib(x) {
+//   let a = 0;
+//   let b = 1;
+//   let result = 0;
+//   for (let i = 2; i <= x; i++) {
+//     result = a + b;
+//     a = b;
+//     b = result;
+//   }
+//   return result;
+// }
+
+//  recursive thinking 
+
+function fib(x){
+    if(x <= 2 ) return 1;
+    return fib(x -1) + fib(x-2);
 }
 
 function fibfunction() {
