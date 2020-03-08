8b319f133170e5e02fadbf9d5e952b15533d1c63
Bronwyn Bonsma <babonsma@gmail.com>
2020-03-08 13:42:24 +0200

    Milestone6

--- js/fab.js
+++ js/fab.js
@@ -18,14 +18,54 @@
 //     return fib(x -1) + fib(x-2);
 // }
 
+// function fibfunction() {
+//   let x = document.getElementById("inputval").value;
+//   console.log(x);
+//   const loader = '<div class="spinner-border" role="status"></div>';
+//   document.getElementById("loaderplacement").innerHTML = loader;
+//   fetch(`http://localhost:5050/fibonacci/${x}`)
+//     .then(resp => resp.json()) // Transform the data into json
+//     .then(function(data) {
+//       let y = data.result;
+//       document.getElementById("demo").innerText = y;
+//       const loaderhidden = '<div class="spinner-border:after" role="status"></div>';
+//       document.getElementById("loaderplacement").innerHTML = loaderhidden;
+//     });
+// }
+
 function fibfunction() {
   let x = document.getElementById("inputval").value;
+  document.getElementById("demo").innerText = "";
+  if (x > 50) {
+    document.getElementById("error").classList.remove("gone");
+    document.getElementById("inputval").classList.add("inputerror");
+  } else {
+    document.getElementById("error").classList.add("gone");
+    document.getElementById("inputval").classList.remove("inputerror");
+    document.getElementById("spinner").classList.remove("loaded");
 
-  fetch(`http://localhost:5050/fibonacci/${x}`)
-    .then(resp => resp.json()) // Transform the data into json
-    .then(function(data) {
-      let y = data.result;
-      document.getElementById("demo").innerText = y;
-    //   console.log(y);
-    });
+    fetch(`http://localhost:5050/fibonacci/${x}`)
+      .then(response => {
+        if (response.ok) {
+          document.getElementById("redError").classList.add("loaded")
+          return response.json();
+        } else {
+          document.getElementById("redError").classList.remove("loaded")
+          throw response;
+        }
+      })
+      .then(function(data) {
+        let y = data.result;
+        document.getElementById("spinner").classList.add("loaded");
+        document.getElementById("demo").innerText = y;
+      })
+
+      .catch(error => error.text())
+      .then(serverError => {
+        console.log(serverError);
+        document.getElementById("spinner").classList.add("loaded")
+        document.getElementById("redError").innerText = serverError;
+      
+      });
+  }
 }
