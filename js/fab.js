0283c9b225989adac3c3434615912b8a0208930c
Bronwyn Bonsma <babonsma@gmail.com>
2020-03-14 22:27:59 +0200

    Final Milestone8

--- js/fab.js
+++ js/fab.js
@@ -27,113 +27,97 @@
 //     });
 // }
 
-
-
-
-let x ;
-
-function fib(x){
-    if (x==0) return 0;
-    if(x <= 2 ) return 1;
-    return fib(x -1) + fib(x-2);
+let x;
+function fib(x) {
+  if (x == 0) return 0;
+  if (x <= 2) return 1;
+  return fib(x - 1) + fib(x - 2);
 }
 
-
-
-  function getX() {
-   x = document.getElementById("inputval").value;
-   console.log(x)
-  document.getElementById("demo").innerText = "";
-  if (x > 50 ) {
+function getX() {
+  x = document.getElementById("inputval").value;
+  console.log(x);
+  document.getElementById("y-result").innerText = "";
+  if (x > 50) {
     document.getElementById("error").classList.remove("gone");
     document.getElementById("inputval").classList.add("inputerror");
   } else {
-    console.log(x)
-    checkbox()
-   
-  
+    console.log(x);
+    checkbox();
   }
 }
 
+function fibfunction() {
+  document.getElementById("error").classList.add("gone");
+  document.getElementById("inputval").classList.remove("inputerror");
+  document.getElementById("spinner").classList.remove("loaded");
+  fetch(`http://localhost:5050/fibonacci/${x}`)
+    .then(response => {
+      if (response.ok) {
+        document.getElementById("redError").classList.add("loaded");
+        return response.json();
+      } else {
+        document.getElementById("redError").classList.remove("loaded");
+        throw response;
+      }
+    })
+    .then(function(data) {
+      let y = data.result;
+      document.getElementById("spinner").classList.add("loaded");
+      document.getElementById("y-result").innerText = y;
+    })
+    .catch(error => error.text())
+    .then(serverError => {
+      console.log(serverError);
+      document.getElementById("spinner").classList.add("loaded");
+      document.getElementById("redError").innerText = serverError;
+    });
+}
 
-function fibfunction(){
-    document.getElementById("error").classList.add("gone");
-    document.getElementById("inputval").classList.remove("inputerror");
-    document.getElementById("spinner").classList.remove("loaded");
-    fetch(`http://localhost:5050/fibonacci/${x}`)
+function displayFibResults() {
+  fetch(`http://localhost:5050/getFibonacciResults`)
+    .then(response => response.json())
+    .then(function fetchResult(data) {
+      console.log(data.results);
+      let dataArray = newArray(data.results);
+      createHistory(dataArray);
+    });
 
-      .then(response => {
-        if (response.ok) {
-          document.getElementById("redError").classList.add("loaded");
-          return response.json();
-        } else {
-          document.getElementById("redError").classList.remove("loaded");
-          throw response;
-        }
-      })
-      .then(function(data) {
-        let y = data.result;
-        document.getElementById("spinner").classList.add("loaded");
-        document.getElementById("demo").innerText = y;
-      })
-
-      .catch(error => error.text())
-      .then(serverError => {
-        console.log(serverError);
-        document.getElementById("spinner").classList.add("loaded");
-        document.getElementById("redError").innerText = serverError;
-        
-      });
+  function newArray(array) {
+    let history = [];
+    for (let i = 0; i < array.length; i++) {
+      let date = new Date(array[i].createdDate);
+      const resultLine = [
+        "The Fibonacci Of <strong>" +
+          array[i].number +
+          "</strong> is <strong>" +
+          array[i].result +
+          "</strong>. Calculated at: " +
+          date +
+          "\n"
+      ];
+      history.push(resultLine);
+    }
+    return history;
   }
 
-
-
-function fibResults(){
-
-fetch(`http://localhost:5050/getFibonacciResults`)
-.then(response => {
-  response.json()
-.then(function(data){
-  // let a = data;
-  // document.getElementById("dataArray").innerText = a;
-   console.log(data);
-
-  for (let i = 0; i < data.results.length; i++) {
-    let myDate= new Date (data.results[i].createdDate)
-    
-
-    // console.log(data.results[i])
-    let element = document.getElementById("dataArray");
-    let line = document.createElement("p");
-    line.innerHTML +=  "The Fibonacci of <b> "  + data.results[i].number  +  " </b>  is <b>  " + data.results[i].result  +  " </b>. calculated at "  +  myDate;
-    element.appendChild(line);
-  }  
-    document.getElementById("loader").classList.add("loaded")
-    
-   
-  });
- 
-  })
+  function createHistory(array) {
+    content.innerHTML = "";
+    for (i = 0; i < array.length; i++) {
+      const arrayItem = document.createElement("p");
+      arrayItem.innerHTML = array[i] + "<hr>";
+      content.appendChild(arrayItem);
+    }
+    document.getElementById("loader").classList.add("loaded");
+  }
 }
-document.addEventListener("DOMContentLoaded", fibResults)
 
+document.addEventListener("DOMContentLoaded", displayFibResults);
 
-
-function checkbox(){
-  if (document.getElementById('checkbox').checked){
-    fibfunction()
+function checkbox() {
+  if (document.getElementById("checkbox").checked) {
+    fibfunction();
   } else {
-    document.getElementById("demo").innerText = fib(x)
+    document.getElementById("y-result").innerText = fib(x);
   }
-
 }
-
-
-
-
-
-
-
-
-
-
